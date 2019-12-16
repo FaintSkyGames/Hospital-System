1bf704bb0899f275046be8bad3c32b299b295956
ninjerie <38001901+ninjerie@users.noreply.github.com>
2019-12-16 22:24:05 +0000

    Created Inital Forms

---
+++ HospitalSytem/src/UserData/Patient.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UserData;
+
+/**
+ *
+ * @author Geri
+ */
+public class Patient extends User {
+    protected int age;
+    protected String gender;
+    
+}
---
+++ HospitalSytem/src/UserData/User.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UserData;
+
+/**
+ *
+ * @author Geri
+ */
+abstract public class User {
+    protected String userType;
+    protected String firstName;
+    protected String surname;
+    protected String ID;
+    protected String address;
+    protected String password;
+    
+    public void logOn(){
+    }
+}
